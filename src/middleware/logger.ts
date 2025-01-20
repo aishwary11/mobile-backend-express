c219badf1d7d5009673f4fdb1451c686421eb9d1
aishwary11 <aishwary46@gmail.com>
2025-01-20 23:32:13 +0530

    logger format change

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,13 +1,20 @@
+import asyncHandler from '@/common/utils/asynchandler';
 import type { NextFunction, Request, Response } from 'express';
-import asyncHandler from '../common/utils/asynchandler';
 
 const logger = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   const start = Date.now();
   res.on('finish', () => {
     const latency = Date.now() - start;
-    console.log(
-      `Time: ${new Date().toLocaleString()}, Method: ${req.method}, originalUrl: ${req.originalUrl}, params: ${JSON.stringify(req.params)}, body: ${JSON.stringify(req.body)}, Latency: ${latency} ms`,
-    );
+    const formattedTime = new Intl.DateTimeFormat('hi-IN', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true,
+    }).format(new Date());
+    console.log(`Time: ${formattedTime}, Method: ${req.method}, originalUrl: ${req.originalUrl}, params: ${JSON.stringify(req.params)}, body: ${JSON.stringify(req.body)}, Latency: ${latency} ms`);
   });
   next();
 });
