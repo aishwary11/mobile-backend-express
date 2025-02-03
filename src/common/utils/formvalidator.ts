6b3d714da21b4be5637e98ac5399ddefc9ee531a
aishwary11 <aishwary46@gmail.com>
2025-02-03 11:23:14 +0530

    type added

--- src/common/utils/formvalidator.ts
+++ src/common/utils/formvalidator.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 import type { Schema } from 'zod';
 import { ZodError } from 'zod';
 import { STATUS_CODES } from '../constant';
