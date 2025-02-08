d51faa49f832cf8cd8f36e69a1f0bc62fcb6d12f
aishwary11 <aishwary46@gmail.com>
2025-02-08 18:40:14 +0530

    rate limit added

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import cors from 'cors';
 import 'dotenv/config';
 import type { Express } from 'express';
 import express from 'express';
+import { rateLimit } from 'express-rate-limit';
 import helmet from 'helmet';
 import pc from 'picocolors';
+import { STATUS_CODES } from './common/constant';
 import connectDB from './common/db/config';
 import userRouter from './components/user/user.route';
 import errorMiddleware from './middleware/error.middleware';
@@ -12,21 +14,25 @@ import logger from './middleware/logger';
 import notFound from './middleware/notFound';
 
 const app: Express = express();
-
 const port: number = parseInt(process.env.PORT!) || 5000;
 
 connectDB();
 
+app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(helmet());
-app.use(logger);
-
+app.use(cors());
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000,
+    limit: 10,
+    message: 'Too many requests from this IP, please try again after a minute.',
+    statusCode: STATUS_CODES.TOO_MANY_REQUESTS,
+  }),
+);
 app.use('/user', userRouter);
 app.use(isAuthenticated);
-
 app.use('*', notFound);
 app.use(errorMiddleware);
-
 app.listen(port, () => console.log(`Server is running on port ${pc.yellow(port)}`));
