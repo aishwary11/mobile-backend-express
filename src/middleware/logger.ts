bcfd22f22fb8c1aa0dc66b48b164464fb93c3ed6
aishwary11 <aishwary46@gmail.com>
2025-06-07 12:19:26 +0530

    package update

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,12 +1,12 @@
 import type { NextFunction, Request, Response } from 'express';
 import pc from 'picocolors';
-import asyncHandler from '../common/utils/asynchandler';
 
-const logger = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  const start = Date.now();
+export default function logger(req: Request, res: Response, next: NextFunction) {
+  const start = process.hrtime.bigint();
   res.on('finish', () => {
-    const latency = Date.now() - start;
-    const formattedTime = new Intl.DateTimeFormat('hi-IN', {
+    const end = process.hrtime.bigint();
+    const latency = Number(end - start) / 1_000_000;
+    const formattedTime = new Date().toLocaleString('hi-IN', {
       year: '2-digit',
       month: '2-digit',
       day: '2-digit',
@@ -14,12 +14,17 @@ const logger = asyncHandler(async (req: Request, res: Response, next: NextFuncti
       minute: '2-digit',
       second: '2-digit',
       hour12: true,
-    }).format(new Date());
+    });
     console.log(
-      `Time: ${pc.green(formattedTime)}, Method: ${pc.yellow(req.method)}, originalUrl: ${pc.blue(req.originalUrl)}, params: ${pc.cyan(JSON.stringify(req.params))}, body: ${pc.cyan(JSON.stringify(req.body))}, Latency: ${pc.red(latency)} ms`,
+      [
+        `Time: ${pc.green(formattedTime)}`,
+        `Method: ${pc.yellow(req.method)}`,
+        `URL: ${pc.blue(req.originalUrl)}`,
+        `Params: ${pc.cyan(JSON.stringify(req.params))}`,
+        `Body: ${pc.cyan(JSON.stringify(req.body))}`,
+        `Latency: ${pc.red(latency.toFixed(2))} ms`
+      ].join(', ')
     );
   });
   next();
-});
-
-export default logger;
+};
